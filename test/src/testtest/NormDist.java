e97e14c7678896bbfcf854406a09404f58405403
Hanseok Kim <hasuikhs@gmail.com>
2020-02-18 17:44:14 +0900

    200218|Modify NormalDistribution

--- test/src/testtest/NormDist.java
+++ test/src/testtest/NormDist.java
@@ -6,20 +6,21 @@ import java.util.List;
 import java.util.Random;
 
 // http://mwultong.blogspot.com/2006/11/java-gaussian-gauss-random-number.html
-public class NormalDistribution {
+public class NormDist {
 	public static void main(String[] args) {
 		Random random = new Random();
-		
+
 		List<Double> list = new ArrayList<Double>();
-		
+
 		for (int i = 0; i < 100; i++) {
-			list.add(random.nextGaussian());
+			list.add(random.nextGaussian() + 3);
 		}
-		
+
 		Collections.sort(list);
 		
-		for(double x : list) {
+		for (double x : list) {
 			System.out.println(x);
 		}
+		
 	}
 }
