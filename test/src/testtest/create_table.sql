880147e664be4b58095dbc40d409b789a0f12115
Hanseok Kim <hasuikhs@gmail.com>
2020-02-18 17:42:52 +0900

    200218|Add sql

---
+++ test/src/testtest/create_table.sql
@@ -0,0 +1,16 @@
+create table reporter(
+    userno int auto_increment,
+    userid varchar(100),
+    activity double,
+    primary key (userno)
+) default character set utf8 collate utf8_general_ci;
+
+create table news(
+    newsno int auto_increment,
+    userno int,
+    date date,
+    title varchar(20),
+    content text,
+    primary key (newsno),
+    foreign key (userno) references reporter (userno)
+) default character set utf8 collate utf8_general_ci;
