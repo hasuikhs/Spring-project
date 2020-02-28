7d33773153a7fac6d65589bbb5189328dac968c7
Hanseok Kim <hasuikhs@gmail.com>
2020-02-28 15:47:51 +0900

    200228|Test Complete

--- test/src/testtest/ListTest.java
+++ test/src/testtest/ListTest.java
@@ -6,6 +6,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.IntStream;
 
 public class ListTest {
 	public static void main(String[] args) {
@@ -22,7 +23,8 @@ public class ListTest {
 				int maxIntervalRange = (int) Math.ceil(usingInternetMonthList.get(i) * 1.1);
 				int minIntervalRange = (int) Math.ceil(usingInternetMonthList.get(i) * 0.9);
 
-				double randomIntervalRange = ((int) ((rand.nextInt((maxIntervalRange - minIntervalRange) + 1) + minIntervalRange + rand.nextDouble()) * 100)) / 10000.0;
+				double randomIntervalRange = ((int) ((rand.nextInt((maxIntervalRange - minIntervalRange) + 1)
+						+ minIntervalRange + rand.nextDouble()) * 100)) / 10000.0;
 				usingInternetMonthList.set(i, randomIntervalRange);
 				prbSum -= randomIntervalRange;
 			} else {
@@ -38,19 +40,21 @@ public class ListTest {
 			monthlyNewsCntList.add((int) Math.ceil(inputCnt * percent));
 			sumCnt += (int) Math.ceil(inputCnt * percent);
 
-			if (sumCnt > inputCnt || (sumCnt < inputCnt
-					&& percent == usingInternetMonthList.get(usingInternetMonthList.size() - 1))) {
+			if ((sumCnt != inputCnt) && (percent == usingInternetMonthList.get(usingInternetMonthList.size() - 1))) {
 				int extra = sumCnt - inputCnt;
 				int randMonth = rand.nextInt(monthlyNewsCntList.size());
 				monthlyNewsCntList.set(randMonth, monthlyNewsCntList.get(randMonth) - extra);
 			}
 		}
-
+		// System.out.println(monthlyNewsCntList);	// [91100, 106800, 79900, 70700, 51500, 85200, 104900, 102000, 84900, 74600, 78300, 70100]
+		// 100만개 확인 완
 		Calendar cal = Calendar.getInstance();
 		int year = 2020;
 
 		List<Double> usingInternetWeekList = Arrays.asList(80.0, 100.0, 100.0, 90.0, 90.0, 90.0, 80.0);
-
+		
+		int testtest = 0;
+		// for 0 ~ 11
 		for (int i = 0; i < monthlyNewsCntList.size(); i++) {
 			int day = 1;
 			cal.set(year, i, day);
@@ -64,6 +68,7 @@ public class ListTest {
 			tempDate.setMonth(i);
 
 			int LastDayOfMonth = cal.getActualMaximum(Calendar.DATE);
+			// 설정된 날부터 말일까지
 			while (day <= LastDayOfMonth) {
 				tempDate.setDate(day);
 
@@ -89,28 +94,80 @@ public class ListTest {
 						int cntCorNewsInt = dailyNewsCntList.get(j) + cntCorNews;
 						sumCntNewsOfDay += cntCorNewsInt;
 						dailyNewsCntList.set(j, cntCorNewsInt);
-						if ((sumCntNewsOfDay > monthlyNewsCnt)
-								|| ((sumCntNewsOfDay < monthlyNewsCnt) && (j == LastDayOfMonth - 1))) {
+						if ((sumCntNewsOfDay != monthlyNewsCnt) && (j == (LastDayOfMonth - 1))) {
 
 							int randDay = rand.nextInt(LastDayOfMonth);
 							int extra = sumCntNewsOfDay - monthlyNewsCnt;
 							dailyNewsCntList.set(randDay, dailyNewsCntList.get(randDay) - extra);
 						}
 					}
-					
-					System.out.println(i + 1 + "월");
-					System.out.println(dailyNewsCntList);
-					
-					List<Double> usingInternetDayList = Arrays.asList(0.0424, 0.0322, 0.0213, 0.0172, 0.0149, 0.0131, 0.013, 0.0167,
-							0.024, 0.04, 0.0447, 0.0474, 0.0459, 0.0487, 0.0471, 0.0569, 0.0675, 0.0711, 0.0596, 0.0555, 0.0499,
-							0.0557, 0.0566, 0.0586);
-					
-					for(int dayOfMonth : dailyNewsCntList) {
-						System.out.println(dayOfMonth);
-					}
 				}
 				day++;
 			}
+			
+			// System.out.println(dailyNewsCntList); // [1868, 1590, 1750, 1683, 1401, 1596, 1988, 1895, 1711, 1650, 1567, 1495, 1545, 1777, 1864, 1636, 1536, 1686, 1356, 1440, 1898, 1725, 1673, 1661, 1685, 1596, 1382, 1720, 1767, 1667, 1792]
+			// 월별 뉴스리스트 확인 완
+
+			// 1 ~ 말일
+			int tDay = 1;
+			for (Integer cntOfDailyNews : dailyNewsCntList) {
+				List<Double> usingInternetDayList = Arrays.asList(0.0424, 0.0322, 0.0213, 0.0172, 0.0149, 0.0131, 0.013,
+						0.0167, 0.024, 0.04, 0.0447, 0.0474, 0.0459, 0.0487, 0.0471, 0.0569, 0.0675, 0.0711, 0.0596,
+						0.0555, 0.0499, 0.0557, 0.0566, 0.0586);
+
+				List<Integer> hourlyNews = new ArrayList<Integer>();
+				for (int k = 0; k < usingInternetDayList.size(); k++) {
+					int standardUsingHour = (int) (usingInternetDayList.get(k) * 10000);
+					int maxUsingHour = (int) Math.ceil(standardUsingHour * 1.02);
+					int minUsingHour = (int) Math.floor(standardUsingHour * 0.98);
+
+					double randomUsingHour = (rand.nextInt((maxUsingHour - minUsingHour) + 1) + minUsingHour) / 10000.0;
+
+					hourlyNews.add((int) Math.round(cntOfDailyNews * randomUsingHour));
+				}
+
+				int sumOfHourNews = 0;
+				for (int k = 0; k < hourlyNews.size(); k++) {
+					sumOfHourNews += hourlyNews.get(k);
+					if ((sumOfHourNews != cntOfDailyNews) && (k == hourlyNews.size() - 1)) {
+						int randomHour = rand.nextInt(hourlyNews.size());
+						int extra = sumOfHourNews - cntOfDailyNews;
+
+						hourlyNews.set(randomHour, hourlyNews.get(randomHour) - extra);
+					}
+				}
+				
+				System.out.println(hourlyNews);
+				
+				int millisTimeOfHour = 3600000;	// 1시간 밀리초
+				
+				System.out.println(year + "년" + i +"월" + tDay + "일");	// i = 0 ~ 11
+				Date curDate = new Date();
+				curDate.setYear(year - 120);
+				curDate.setMonth(i);
+				curDate.setDate(tDay);
+				curDate.setMinutes(0);
+				curDate.setSeconds(0);
+				for (int k = 0; k < hourlyNews.size(); k++) {
+					int sumIntervalTime = 0;
+					
+					// k = 0 ~ 23 시 지정
+					curDate.setHours(k);
+					int interTime = millisTimeOfHour / hourlyNews.get(k);
+
+					for (int l = 0; l < hourlyNews.get(k); l++) {
+						Date tmpDate = new Date();
+						tmpDate.setTime(curDate.getTime() + sumIntervalTime);
+						System.out.println(tmpDate);
+						
+						sumIntervalTime += interTime;
+						testtest++;
+					}
+					
+				}
+				tDay++;
+			}
 		}
+		
 	}
 }
